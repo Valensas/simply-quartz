d5bc71aff56eff579826da0bdc0109a4f4d36dd7
ofarukdogancay <ofarukdogancay@gmail.com>
2024-04-18 13:01:30 +0300

    Removed unnecessary configuration annotation

--- src/main/kotlin/com/valensas/simplyquartz/SimplyQuartzProperties.kt
+++ src/main/kotlin/com/valensas/simplyquartz/SimplyQuartzProperties.kt
@@ -1,9 +1,7 @@
 package com.valensas.simplyquartz
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties("simplyquartz")
 class SimplyQuartzProperties {
     var enabled: Boolean = true
