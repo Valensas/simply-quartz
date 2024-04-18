fa584448ed11a03464cc9c457bee9bb92478c794
ofarukdogancay <ofarukdogancay@gmail.com>
2024-04-18 10:57:06 +0300

    Enabled spring boot, disabled processAot for kotlinter to work without a main function

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    id("org.springframework.boot") version "3.2.4" apply false
+    id("org.springframework.boot") version "3.2.4"
     id("org.jmailen.kotlinter") version "4.2.0"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.graalvm.buildtools.native") version "0.9.28"
@@ -60,4 +60,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named("processAot") {
+    enabled = false
 }
