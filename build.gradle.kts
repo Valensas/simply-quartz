432964f1ef6ba6cc371549cdb5b8a2dfe2cf4c4e
ofarukdogancay <ofarukdogancay@gmail.com>
2024-04-18 19:20:42 +0300

    Added central portal publisher dependencies, renamed workflow files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,17 @@ plugins {
     id("org.jmailen.kotlinter") version "4.2.0"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.graalvm.buildtools.native") version "0.9.28"
+
+    id("maven-publish")
+    id("java-library")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
+
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "com.valensas"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -48,4 +53,41 @@ tasks.withType<KotlinCompile> {
 
 tasks.named("processAot") {
     enabled = false
+}
+
+signing {
+    val keyId = System.getenv("SIGNING_KEYID")
+    val secretKey = System.getenv("SIGNING_SECRETKEY")
+    val passphrase = System.getenv("SIGNING_PASSPHRASE")
+
+    useInMemoryPgpKeys(keyId, secretKey, passphrase)
+}
+
+centralPortal {
+    username = System.getenv("SONATYPE_USERNAME")
+    password = System.getenv("SONATYPE_PASSWORD")
+
+    pom {
+        name = "Exposed Extras"
+        description = "An utility library that adds features to Exposed ORM"
+        url = "https://valensas.com/"
+        scm {
+            url = "https://github.com/Valensas/exposed-extras"
+        }
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://mit-license.org")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("0")
+                name.set("Valensas")
+                email.set("info@valensas.com")
+            }
+        }
+    }
 }
