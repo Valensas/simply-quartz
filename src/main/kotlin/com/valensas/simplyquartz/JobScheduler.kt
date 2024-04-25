9a8805b8fdfbfe88f2a62a033cf05367b73c745a
ofarukdogancay <ofarukdogancay@gmail.com>
2024-04-25 17:28:01 +0300

    Simplified annotation parameters and parsing logic

--- src/main/kotlin/com/valensas/simplyquartz/JobScheduler.kt
+++ src/main/kotlin/com/valensas/simplyquartz/JobScheduler.kt
@@ -19,6 +19,7 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.EventListener
+import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 // Need to get the main application class to determine the root package for job scanning at runtime
@@ -67,7 +68,18 @@ class JobScheduler(
                     resolveEnvironmentPlaceholders(scheduleAnnotation.jobGroup)
                         .ifBlank { jobClass.`package`.name }
 
-                handleAnnotation(scheduleAnnotation, jobClass, jobName, jobGroup, newJobKeysSet)
+                val jobKey = JobKey.jobKey(jobName, jobGroup)
+
+                val enabled = resolveEnvironmentPlaceholders(scheduleAnnotation.enabled).toBoolean()
+
+                if (!enabled) {
+                    logger.info("Job $jobKey is disabled")
+                    return@let
+                }
+
+                handleAnnotation(scheduleAnnotation, jobClass, jobKey)
+
+                newJobKeysSet.add(jobKey)
             }
         }
 
@@ -94,59 +106,29 @@ class JobScheduler(
             ?: throw IllegalStateException("Unable to determine base package for job scanning")
     }
 
-    private fun handleAnnotation(scheduleAnnotation: QuartzSchedule, jobClass: Class<out Job>, jobName: String, jobGroup: String, newJobKeysSet: MutableSet<JobKey>) {
-        val jobKey = JobKey.jobKey(jobName, jobGroup)
-
+    private fun handleAnnotation(
+        scheduleAnnotation: QuartzSchedule,
+        jobClass: Class<out Job>,
+        jobKey: JobKey
+    ) {
         val cronExpression = resolveEnvironmentPlaceholders(scheduleAnnotation.cron)
 
-        val fixedDelayString = resolveEnvironmentPlaceholders(scheduleAnnotation.fixedDelayString)
+        val fixedDelayString = resolveEnvironmentPlaceholders(scheduleAnnotation.fixedDelay)
 
-        val initialDelayString = resolveEnvironmentPlaceholders(scheduleAnnotation.initialDelayString)
+        val initialDelayString = resolveEnvironmentPlaceholders(scheduleAnnotation.initialDelay)
 
-        val fixedDelay = scheduleAnnotation.fixedDelay
-
-        val initialDelay = scheduleAnnotation.initialDelay
-
-        val fixedDelayParameterSet = fixedDelayString != "" || fixedDelay != -1L
-        val initialDelayParameterSet = initialDelayString != "" || initialDelay != -1L
-        val cronParameterSet = cronExpression != ""
-
-        if (fixedDelayParameterSet && cronParameterSet) {
-            throw IllegalArgumentException("Both fixed delay and cron parameters are set for job $jobName")
+        if (fixedDelayString.isNotBlank() && cronExpression.isNotBlank()) {
+            throw IllegalArgumentException("Both fixed delay and cron parameters are set for job $jobKey")
         }
 
-        if (initialDelayParameterSet && cronParameterSet) {
-            throw IllegalArgumentException("Both initial delay and cron parameters are set for job $jobName")
-        }
+        if (fixedDelayString.isNotBlank()) {
+            var finalFixedDelay =
+                fixedDelayString.toLongOrNull().takeIf { it != null } ?: Duration.parse(fixedDelayString).toMillis()
+            var finalInitialDelay =
+                initialDelayString.toLongOrNull().takeIf { it != null } ?: Duration.parse(initialDelayString).toMillis()
 
-        if (fixedDelayParameterSet) {
-            var finalFixedDelay = if (fixedDelayString != "") {
-                fixedDelayString.toLongOrNull()
-            } else {
-                fixedDelay
-            }
-
-            if (finalFixedDelay == null) {
-                throw IllegalArgumentException("Invalid fixed delay string value for job $jobName")
-            }
-
-            if (finalFixedDelay == -1L) {
-                logger.info("Skipping disabled fixed delay job $jobName")
-                return
-            }
-
-            var finalInitialDelay = if (initialDelayString != "") {
-                initialDelayString.toLongOrNull()
-            } else {
-                initialDelay
-            }
-
-            if (finalInitialDelay == null) {
-                throw IllegalArgumentException("Invalid initial delay string value for job $jobName")
-            }
-
-            if (finalInitialDelay == -1L) {
-                finalInitialDelay = 0L
+            if (finalFixedDelay < 0) {
+                throw IllegalArgumentException("Fixed delay must be a positive number for job $jobKey")
             }
 
             if (scheduleAnnotation.timeUnit != TimeUnit.MILLISECONDS) {
@@ -154,38 +136,31 @@ class JobScheduler(
                 finalInitialDelay = scheduleAnnotation.timeUnit.toMillis(finalInitialDelay)
             }
 
-            scheduleFixedDelayJob(jobClass, jobName, jobGroup, finalInitialDelay, finalFixedDelay)
-        } else if (cronParameterSet) {
-            if (cronExpression == scheduleAnnotation.CRON_DISABLED) {
-                logger.info("Skipping disabled cron job $jobName")
-                return
-            }
-
-            scheduleCronJob(jobClass, jobName, jobGroup, cronExpression)
+            scheduleFixedDelayJob(jobClass, jobKey, finalInitialDelay, finalFixedDelay)
+        } else if (cronExpression.isNotBlank()) {
+            scheduleCronJob(jobClass, jobKey, cronExpression)
         } else {
-            throw IllegalArgumentException("No scheduling parameters provided for job $jobName")
+            throw IllegalArgumentException("No scheduling parameters provided for job $jobKey")
         }
-
-        newJobKeysSet.add(jobKey)
     }
 
     private fun scheduleFixedDelayJob(
         jobClass: Class<out Job>?,
-        jobName: String?,
-        jobGroup: String?,
+        jobKey: JobKey,
         finalInitialDelay: Long,
         finalFixedDelay: Long
     ) {
-        val jobKey = JobKey.jobKey(jobName, jobGroup)
         val newTrigger = TriggerBuilder.newTrigger()
-            .withIdentity(jobName, jobGroup)
-            .forJob(jobName, jobGroup)
-            .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInMilliseconds(finalFixedDelay).repeatForever())
+            .withIdentity(jobKey.name, jobKey.group)
+            .forJob(jobKey)
+            .withSchedule(
+                SimpleScheduleBuilder.simpleSchedule().withIntervalInMilliseconds(finalFixedDelay).repeatForever()
+            )
             .startAt(java.util.Date(System.currentTimeMillis() + finalInitialDelay))
             .build()
         val jobDetail = JobBuilder.newJob(jobClass)
             .requestRecovery(false)
-            .withIdentity(jobName, jobGroup)
+            .withIdentity(jobKey)
             .storeDurably()
             .build()
 
@@ -196,16 +171,15 @@ class JobScheduler(
         }
     }
 
-    private fun scheduleCronJob(clazz: Class<out Job>, jobName: String, jobGroup: String, cronExpression: String) {
-        val jobKey = JobKey.jobKey(jobName, jobGroup)
+    private fun scheduleCronJob(clazz: Class<out Job>, jobKey: JobKey, cronExpression: String) {
         val newTrigger = TriggerBuilder.newTrigger()
-            .withIdentity(jobName, jobGroup)
-            .forJob(jobName, jobGroup)
+            .withIdentity(jobKey.name, jobKey.group)
+            .forJob(jobKey)
             .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
             .build()
         val jobDetail = JobBuilder.newJob(clazz)
             .requestRecovery(false)
-            .withIdentity(jobName, jobGroup)
+            .withIdentity(jobKey)
             .storeDurably()
             .build()
 
--- src/main/kotlin/com/valensas/simplyquartz/QuartzSchedule.kt
+++ src/main/kotlin/com/valensas/simplyquartz/QuartzSchedule.kt
@@ -1,26 +1,20 @@
 package com.valensas.simplyquartz
 
-import org.springframework.scheduling.annotation.Scheduled
 import java.util.concurrent.TimeUnit
 
-@Scheduled()
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class QuartzSchedule(
 
     val cron: String = "",
 
-    val fixedDelay: Long = -1L,
+    val fixedDelay: String = "",
 
-    val fixedDelayString: String = "",
-
-    val initialDelay: Long = -1L,
-
-    val initialDelayString: String = "",
+    val initialDelay: String = "",
 
     val timeUnit: TimeUnit = TimeUnit.MILLISECONDS,
 
-    val CRON_DISABLED: String = "-",
+    val enabled: String = "",
 
     val jobName: String = "",
 
