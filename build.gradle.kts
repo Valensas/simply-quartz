4413c629c549b5f2d9b3b1168fa485141b01e036
ofarukdogancay <ofarukdogancay@gmail.com>
2024-04-25 09:48:57 +0300

    Fixed jar task naming issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "com.valensas"
-version = "0.1.0"
+version = "0.1.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -41,6 +41,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-quartz:3.2.0")
     implementation("org.reflections:reflections:0.10.2")
+}
+
+tasks.getByName<Jar>("jar") {
+    archiveClassifier.set("")
 }
 
 tasks.withType<KotlinCompile> {
